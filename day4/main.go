29e2bcac6101bd3cf95b6a5be5afee2c8cf7d770
Dániel Jávorszky <daniel.javorszky@gmail.com>
2017-12-04 22:25:49 +0100

    day4 part2

--- day4/main.go
+++ day4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
 
@@ -10,8 +11,6 @@ func main() {
 	b, _ := ioutil.ReadFile("input.txt")
 
 	lines := strings.Split(string(b), "\r\n")
-
-	fmt.Println(lines[len(lines)-1])
 
 	valid := 0
 	for _, line := range lines {
@@ -27,6 +26,11 @@ func isValid(pass string) bool {
 	seen := make(map[string]bool)
 
 	for _, str := range strings.Split(pass, " ") {
+		runes := []rune(str)
+		sort.Sort(byRunes(runes))
+
+		str = string(runes)
+
 		if _, ok := seen[str]; ok {
 			return false
 		}
@@ -35,4 +39,18 @@ func isValid(pass string) bool {
 	}
 
 	return true
+}
+
+type byRunes []rune
+
+func (s byRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s byRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byRunes) Len() int {
+	return len(s)
 }
